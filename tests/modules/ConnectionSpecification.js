019f904fc0f0038aa9ccd30b6699d852a19afa4d
Nick Dugger <email@nickdugger.com>
2018-06-20 13:17:31 -0500

    Update ConnectionSpecification.js

--- tests/modules/ConnectionSpecification.js
+++ tests/modules/ConnectionSpecification.js
@@ -16,21 +16,23 @@ module.exports = new Specification('Connection', test => {
     });
     
     test.case('upon connection, should save socket via this.establish', () => {
-        const socket = new Spy();
+        const connectionSpy = new Spy(connection);
+        const socketSpy = new Spy();
         
-        server.emit('connection', [ socket ]);
+        server.emit('connection', [ socketSpy ]);
         
-        test.expect(test.called(socket.on));
-        test.expect(connection.sockets.includes(socket));
+        test.espect(test.called(connectionSpy.establish).with(socketSpy));
+        test.expect(test.called(socketSpy.on));
+        test.expect(connection.sockets.includes(socketSpy));
     });
     
     test.case('should allow sending messages as JSON to connections', () => {
-        const socket = new Spy();
+        const socketSpy = new Spy();
         const tasks = [ { name: 'foo' } ];
         
-        connection.sockets = [ socket ];
+        connection.sockets = [ socketSpy ];
         connection.send(tasks);
         
-        test.expect(test.called(socket.send).with(JSON.stringify(tasks)));
+        test.expect(test.called(socketSpy.send).with(JSON.stringify(tasks)));
     });
 });
