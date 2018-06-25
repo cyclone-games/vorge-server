7ad4dd4305b57abce494edd057f22784c00d4329
Nick Dugger <email@nickdugger.com>
2018-06-24 22:19:36 -0500

    danksnack

--- modules/Router.js
+++ modules/Router.js
@@ -11,10 +11,14 @@ const util = {
     file (status, file) {
         const type = fileType(file);
         const mime = type ? type.mime : 'text/plain';
+        const length = Buffer.byteLength(file);
 
         return {
             status: status,
-            headers: { 'content-type': mime },
+            headers: {
+                'content-type': mime,
+                'content-length': length
+            },
             body: file
         };
     },
@@ -64,41 +68,41 @@ module.exports = class Router extends Module {
     }
 
     async handle (request, response) {
-        const method = request.method.toLowerCase();
         const { pathname } = url.parse(request.url);
         const route = this.juncture.find(request.method, pathname);
 
         response.setHeader('access-control-allow-origin', '*');
-        response.setHeader('access-control-allow-headers', 'content-type, authorization, *');
+        response.setHeader('access-control-allow-headers', 'content-type, content-length, authorization, *');
         response.setHeader('access-control-allow-methods', 'get, post, put, delete, *');
+        response.setHeader('access-control-expose-headers', 'content-type, content-length, authorization, *');
 
-        if (method === 'options') {
+        if (request.method.toLowerCase() === 'options') {
             response.end();
         }
         else if (!route) {
             const error = util.error(404, 'Not Found');
 
+            this.server.logger.error(JSON.parse(error.message).error);
+
             response.writeHead(error.code, error.meta);
             response.end(error.message);
-
-            this.server.logger.error(JSON.parse(error.message).error);
         }
 
         if (!response.finished) try {
             const result = await route.action.call(this, request, route.params);
 
+            this.server.logger.info(`${ request.method.toLowerCase() } ${ pathname }`);
+
             response.writeHead(result.status, result.headers);
             response.end(result.body);
-
-            this.server.logger.info(`${ method } ${ pathname }`);
         }
         catch (error) {
             const code = Number.parseInt(error.code) || 500;
 
+            this.server.logger.error(JSON.parse(error.message).error);
+
             response.writeHead(code, error.meta);
             response.end(error.message);
-
-            this.server.logger.error(JSON.parse(error.message).error);
         }
     }
 };
