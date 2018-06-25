e63c8bbbb3ce4feff289db1572815083c8cb3a84
Nick Dugger <email@nickdugger.com>
2018-06-24 23:28:01 -0500

    a

--- plugins/common/tasks/handshake.js
+++ plugins/common/tasks/handshake.js
@@ -1,3 +1,23 @@
 module.exports = function handshake (origin) {
-    
+    const std = this.libraries.use('std');
+    const entity = std.entities.player.create({
+        experience: 0,
+        health: {
+            now: 100,
+            max: 100
+        },
+        name: 'admin',
+        position: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+        texture: {
+            data: null,
+            fill: false,
+            clip: null
+        }
+    });
+
+    console.log(std, entity);
 };
